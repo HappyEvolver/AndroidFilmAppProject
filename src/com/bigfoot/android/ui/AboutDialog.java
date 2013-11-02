c00efb16f0445e668df4e27c0d0920ee2495937d
Neil Pattinson <neil@bigfoot-computing.co.uk>
2013-11-02 09:57:19 +0000

    Removed redundant import

--- src/com/bigfoot/android/ui/AboutDialog.java
+++ src/com/bigfoot/android/ui/AboutDialog.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.androidquery.util.AQUtility;
 import com.bigfoot.android.R;
 
 /**
